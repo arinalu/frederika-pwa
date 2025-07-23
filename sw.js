7fbc1639ec669bead7142d11b277068d71dabba0
arinalu <154529227+arinalu@users.noreply.github.com>
2025-07-23 11:55:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,14 +10,29 @@ const urlsToCache = [
   '/icon2.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) =>
+            cache.addAll(URLS_TO_CACHE))
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', function (event) {
+    event.respondWith(fromCache(event.request));
+    event.waitUntil(update(event.request));
 });
+
+function fromCache(request) {
+    return caches.open(CACHE_NAME).then((cache) =>
+        cache.match(request).then((matching) =>
+            matching || Promise.reject('no-match')
+        ));
+}
+
+function update(request) {
+    return caches.open(CACHE_NAME).then((cache) =>
+        fetch(request).then((response) =>
+            cache.put(request, response)
+        )
+    );
+}
